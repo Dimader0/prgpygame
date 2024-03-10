bc77802c9cda68e7e8b5360abd532fa59af35840
Gotliha <145850039+Gotliha@users.noreply.github.com>
2024-03-10 16:41:40 +0200

    6

--- shooter/shooter-game-main/shooter/shooter_game.py
+++ shooter/shooter-game-main/shooter/shooter_game.py
@@ -1,3 +1,19 @@
 #Створи власний Шутер!
 
 from pygame import *
+from time import sleep
+
+class GameSprite(sprite.Sprite):
+    def __init__ (self, img, x, y, w, h):
+        super().__init__()
+        self.image = transform.scale(image.load(img), (w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+screen = display.set_mode((700, 500))
+display.set_caption("shooter")
+bg = transform.scale(image.load("galaxy.jpg"), (700, 500))

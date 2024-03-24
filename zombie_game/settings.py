e5845c1923c5303698b7ac97e4516b16c79ab2b9
Gotliha <145850039+Gotliha@users.noreply.github.com>
2024-03-24 16:34:17 +0200

    6

--- zombie_game/settings.py
+++ zombie_game/settings.py
@@ -14,3 +14,31 @@ clock = time.Clock()
 #Зоображення
 player_image = './textures/player.png'
 zombie_images = ['./textures/zombie1.png', './textures/zombie2.png', './textures/zombie3.png']
+bullet_image = './textures/bullet.png'
+
+#Фонове зоображення
+background_imaage = transform.scale(image.load('./textures/background.png'), (win_width, win_height))
+
+#Звуки
+fire_sound = mixer.Sound('./sounds/fire.ogg')
+coin_sound = mixer.Sound('./sounds/coin.ogg')
+coins_sound = mixer.Sound('./sounds/coins.ogg')
+damage_sound = mixer.Sound('./sounds/damage.ogg')
+death_sound = mixer.Sound('./sounds/death.ogg')
+
+#Музика
+mixer.music.load('./sounds/music.mp3')
+
+#Колір фону інтерфейса
+background = (150, 150, 100)
+
+#Групи для куль та ворогів
+bullets = sprite.Group()
+zombies = sprite.Group()
+
+#Шрифт інтерфейса
+ui_font = font.Font(None, 50)
+
+#Інтерфейс
+UI = Rect(0, win_height, win_width, 50)
+
